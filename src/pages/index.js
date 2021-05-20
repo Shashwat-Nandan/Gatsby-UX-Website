befdd57d6c219cd250dad706fa2e398cc78abfbb
Shashwat-Nandan <shashwat.nandan@propelytics.in>
2021-05-20 19:15:08 +0530

    about page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
 function IndexPage() {
-  return <div>This is the home page</div>;
+  return (
+    <>
+      <h1>Propelytics Home</h1>
+      <div>This is the home page</div>
+      <Link to="/about/">About Us</Link>
+    </>
+  );
 }
 
 export default IndexPage;
