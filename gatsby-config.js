681c0410059505ca0d4c283b4d005e8bd5f2a66e
Shashwat-Nandan <shashwat.nandan@propelytics.in>
2021-05-28 18:52:26 +0530

    Add config for Mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/content`,
+        name: `content`,
+      },
+    },
   ],
 };
