14925cf1cfcf130d72b8063cf7235b056be2bf71
Shashwat-Nandan <shashwat.nandan@propelytics.in>
2021-05-28 23:22:15 +0530

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,20 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Box, Heading } from "theme-ui";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <>
+      <Heading>
+        Page not found!
+        <span role="img" aria-label="crying face">
+          :(
+        </span>
+      </Heading>
+      <Box as="h2" variant="styles.h2">
+        It looks like the page does not exist
+      </Box>
+    </>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
